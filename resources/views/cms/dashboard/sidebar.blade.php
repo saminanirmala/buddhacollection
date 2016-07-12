fea847652a44c65d2832327d4a71577e9d3bd0d2
samina <samina@ekbana.com>
2016-07-12 17:32:06 +0545

    files added

---
+++ resources/views/cms/dashboard/sidebar.blade.php
@@ -0,0 +1,22 @@
+<ul class="nav navbar-nav side-nav">
+    <li class="active">
+        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+    </li>
+    <li>
+        <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
+    </li>
+    <li>
+        <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
+    </li>
+    <li>
+        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+        <ul id="demo" class="collapse">
+            <li>
+                <a href="#">Dropdown Item</a>
+            </li>
+            <li>
+                <a href="#">Dropdown Item</a>
+            </li>
+        </ul>
+    </li>
+</ul>
